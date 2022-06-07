8cff5bf4afaa912735fc2bf2cfd644ccafe14020
Mike <mike@failsafedesign.com>
2022-06-06 21:36:03 -0700

    Add Grid compeont to visualize vertical rythm

---
+++ app/components/Grid.tsx
@@ -0,0 +1,10 @@
+export const Grid = () => {
+  return (
+    <div className="absolute top-0 left-0 right-0 overflow-hidden z-0">
+      {[...Array(100)].map((e, index) => (
+        <div key={`grid_${index}`} className="h-2 border-red-500 border-b opacity-50" />
+      ))}
+      =
+    </div>
+  );
+};
